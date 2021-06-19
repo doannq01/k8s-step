8515db663f24b0b80f2fa3005161462a3b96d7b3
Quoc Doan Nguyen <doannq01@gmail.com>
2021-06-20 00:08:54 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "***. Deploy ***"
-/usr/local/bin/docker container stop pedantic_bell
+/usr/local/bin/docker container stop k8s_step
 
-/usr/local/bin/docker run -d -p 5000:80 image:$GIT_COMMIT
+/usr/local/bin/docker run -d -p 5000:80 --name k8s_step image:$GIT_COMMIT
